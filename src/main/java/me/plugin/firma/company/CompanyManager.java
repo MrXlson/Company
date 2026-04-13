547181b210de1e131c1636024d3e7920afa312d9
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 14:48:46 +0200

    CompanyManager.java

--- src/main/java/me/plugin/firma/company/CompanyManager.java
+++ src/main/java/me/plugin/firma/company/CompanyManager.java
@@ -1,1 +1,32 @@
+package me.plugin.firma.company;
 
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CompanyManager {
+
+    private Map<String, Company> companies = new HashMap<>();
+
+    public Company createCompany(String name, Player p) {
+        Company c = new Company(name, p.getUniqueId());
+        companies.put(name, c);
+        return c;
+    }
+
+    public Company getCompany(Player p) {
+        for (Company c : companies.values()) {
+            if (c.members.containsKey(p.getUniqueId())) return c;
+        }
+        return null;
+    }
+
+    public Map<String, Company> getCompanies() {
+        return companies;
+    }
+
+    public void setCompanies(Map<String, Company> companies) {
+        this.companies = companies;
+    }
+}
