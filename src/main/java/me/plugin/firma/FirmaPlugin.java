6332f53e8e73f0f65135be4ea7111e325f7804f3
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 22:32:52 +0200

    FirmaPlugin.java

--- src/main/java/me/plugin/firma/FirmaPlugin.java
+++ src/main/java/me/plugin/firma/FirmaPlugin.java
@@ -1,7 +1,10 @@
 package me.plugin.firma;
 
 import me.plugin.firma.command.FirmaCommand;
-import me.plugin.firma.listener.*;
+import me.plugin.firma.listener.ChatListener;
+import me.plugin.firma.listener.FirmaListener;
+import me.plugin.firma.listener.JobListener;
+import me.plugin.firma.listener.QuestListener;
 import me.plugin.firma.manager.FirmaManager;
 
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,26 +16,31 @@ public class FirmaPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
 
+        // 📦 config
         saveDefaultConfig();
 
+        // 🧠 manager
         manager = new FirmaManager(this);
 
-        // COMMAND
+        // ⚙️ command
         getCommand("firma").setExecutor(new FirmaCommand(manager));
 
-        // LISTENERY
+        // 🎧 listenery
         getServer().getPluginManager().registerEvents(new FirmaListener(manager), this);
         getServer().getPluginManager().registerEvents(new ChatListener(manager), this);
         getServer().getPluginManager().registerEvents(new QuestListener(manager), this);
-
-        // 🔥 NOVÉ (PRÁCE)
         getServer().getPluginManager().registerEvents(new JobListener(manager), this);
 
-        getLogger().info("BizCore FULL loaded!");
+        getLogger().info("§aBizCore plně načten!");
     }
 
     @Override
     public void onDisable() {
         saveConfig();
+        getLogger().info("§cBizCore vypnut!");
+    }
+
+    public FirmaManager getManager() {
+        return manager;
     }
 }
