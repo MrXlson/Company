a51477ff05cd7628dbc4a617c7a293ab5560ce95
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 18:40:16 +0200

    ChatInputManager.java

--- src/main/java/me/plugin/firma/chat/ChatInputManager.java
+++ src/main/java/me/plugin/firma/chat/ChatInputManager.java
@@ -1,1 +1,21 @@
+package me.plugin.firma.chat;
 
+import java.util.HashSet;
+import java.util.UUID;
+
+public class ChatInputManager {
+
+    private static final HashSet<UUID> waiting = new HashSet<>();
+
+    public static void add(UUID uuid) {
+        waiting.add(uuid);
+    }
+
+    public static boolean isWaiting(UUID uuid) {
+        return waiting.contains(uuid);
+    }
+
+    public static void remove(UUID uuid) {
+        waiting.remove(uuid);
+    }
+}
