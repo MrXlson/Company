d8e634563fb51ab49b57a388bbe8c59fb8aa9171
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 19:50:43 +0200

    UpgradeGUI.java

--- src/main/java/me/plugin/firma/gui/UpgradeGUI.java
+++ src/main/java/me/plugin/firma/gui/UpgradeGUI.java
@@ -1,54 +1,32 @@
 package me.plugin.firma.gui;
 
 import me.plugin.firma.manager.FirmaManager;
-
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.*;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.event.inventory.InventoryClickEvent;
 
 import java.util.Arrays;
 
 public class UpgradeGUI {
 
-    public static void open(Player player, FirmaManager manager) {
+    public static void open(Player p, FirmaManager manager) {
 
-        String firma = manager.getCompany(player.getUniqueId());
+        Inventory inv = Bukkit.createInventory(null, 27, "§aUpgrady");
 
-        Inventory inv = Bukkit.createInventory(null, 27, "§6Upgrade Shop");
+        String f = manager.getCompany(p.getUniqueId());
 
         ItemStack multi = new ItemStack(Material.EMERALD);
-        ItemMeta mm = multi.getItemMeta();
-        mm.setDisplayName("§aMultiplier Upgrade");
-        mm.setLore(Arrays.asList(
-                "§7Aktuální: " + manager.getMultiplier(firma),
-                "§6Cena: 1000"
+        ItemMeta m = multi.getItemMeta();
+        m.setDisplayName("§aMultiplier");
+        m.setLore(Arrays.asList(
+                "§7Aktuální: " + manager.getMultiplier(f),
+                "§7Cena: 1000"
         ));
-        multi.setItemMeta(mm);
+        multi.setItemMeta(m);
 
-        inv.setItem(11, multi);
+        inv.setItem(13, multi);
 
-        player.openInventory(inv);
-    }
-
-    public static void handleClick(Player player, InventoryClickEvent e, FirmaManager manager) {
-
-        String firma = manager.getCompany(player.getUniqueId());
-        String name = e.getCurrentItem().getItemMeta().getDisplayName();
-
-        if (name.equals("§aMultiplier Upgrade")) {
-
-            if (manager.getBalance(firma) < 1000) {
-                player.sendMessage("§cMálo peněz!");
-                return;
-            }
-
-            manager.removeBalance(firma, 1000);
-            manager.upgradeMultiplier(firma);
-
-            player.sendMessage("§aUpgrade koupen!");
-        }
+        p.openInventory(inv);
     }
 }
