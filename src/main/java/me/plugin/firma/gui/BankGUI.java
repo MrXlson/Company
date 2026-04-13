f20e306513ddd50e6c19a3ab85af4c6a17f4347b
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 15:31:10 +0200

    BankGUI.java

--- src/main/java/me/plugin/firma/gui/BankGUI.java
+++ src/main/java/me/plugin/firma/gui/BankGUI.java
@@ -1,29 +1,31 @@
 package me.plugin.firma.gui;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
+import me.plugin.firma.company.Company;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.*;
+
+import java.util.List;
 
 public class BankGUI {
 
-    public static void open(Player p) {
+    public static void open(Player p, Company c) {
 
         Inventory inv = Bukkit.createInventory(null, 27, "§bBanka");
 
-        inv.setItem(11, item(Material.GREEN_WOOL, "§aVložit +100"));
-        inv.setItem(13, item(Material.RED_WOOL, "§cVybrat -100"));
-        inv.setItem(15, item(Material.BARRIER, "§cZpět"));
+        inv.setItem(11, item(Material.GREEN_WOOL, "§aVložit"));
+        inv.setItem(13, item(Material.RED_WOOL, "§cVybrat"));
+
+        inv.setItem(22, item(Material.EMERALD, "§aBalance", "§7" + c.balance));
 
         p.openInventory(inv);
     }
 
-    private static ItemStack item(Material m, String name) {
+    private static ItemStack item(Material m, String name, String... lore) {
         ItemStack i = new ItemStack(m);
-        ItemMeta meta = i.getItemMeta();
+        var meta = i.getItemMeta();
         meta.setDisplayName(name);
+        meta.setLore(List.of(lore));
         i.setItemMeta(meta);
         return i;
     }
