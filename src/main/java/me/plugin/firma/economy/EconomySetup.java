41bd0c1c4170584ba75877ea509f6fcb7d55f63e
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 18:33:54 +0200

    EconomySetup.java

--- src/main/java/me/plugin/firma/economy/EconomySetup.java
+++ src/main/java/me/plugin/firma/economy/EconomySetup.java
@@ -1,1 +1,28 @@
+package me.plugin.firma.economy;
 
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EconomySetup {
+
+    private static Economy econ;
+
+    public static boolean setup(JavaPlugin plugin) {
+        if (plugin.getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+
+        RegisteredServiceProvider<Economy> rsp =
+                plugin.getServer().getServicesManager().getRegistration(Economy.class);
+
+        if (rsp == null) return false;
+
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public static Economy get() {
+        return econ;
+    }
+}
