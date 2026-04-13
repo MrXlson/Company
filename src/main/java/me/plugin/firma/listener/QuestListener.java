4de0f1ba8058adb6d3acbb7e43f258984c9d9b56
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 18:21:09 +0200

    QuestListener.java

--- src/main/java/me/plugin/firma/listener/QuestListener.java
+++ src/main/java/me/plugin/firma/listener/QuestListener.java
@@ -1,1 +1,36 @@
+package me.plugin.firma.listener;
 
+import me.plugin.firma.manager.*;
+
+import org.bukkit.event.*;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.entity.Player;
+
+public class QuestListener implements Listener {
+
+    private final FirmaManager manager;
+
+    public QuestListener(FirmaManager manager) {
+        this.manager = manager;
+    }
+
+    @EventHandler
+    public void onKill(PlayerDeathEvent e) {
+
+        Player killer = e.getEntity().getKiller();
+        if (killer == null) return;
+
+        String firma = manager.getCompany(killer.getUniqueId());
+        if (firma == null) return;
+
+        QuestManager.add(firma);
+
+        if (QuestManager.get(firma) >= 10) {
+            manager.addBalance(firma, 500);
+            manager.addXP(firma, 50);
+            QuestManager.reset(firma);
+
+            killer.sendMessage("§aQuest splněn!");
+        }
+    }
+}
