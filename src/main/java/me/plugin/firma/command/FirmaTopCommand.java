37684c0a8c29e459d7997bbdb026e769f5cd605a
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 18:18:57 +0200

    FirmaTopCommand.java

--- src/main/java/me/plugin/firma/command/FirmaTopCommand.java
+++ src/main/java/me/plugin/firma/command/FirmaTopCommand.java
@@ -1,1 +1,38 @@
+package me.plugin.firma.command;
 
+import me.plugin.firma.manager.FirmaManager;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+
+import java.util.*;
+
+public class FirmaTopCommand implements CommandExecutor {
+
+    private final FirmaManager manager;
+
+    public FirmaTopCommand(FirmaManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (!(sender instanceof Player)) return true;
+        Player player = (Player) sender;
+
+        Map<String, Double> map = new HashMap<>();
+
+        for (String f : manager.getPlugin().getConfig().getConfigurationSection("firma").getKeys(false)) {
+            map.put(f, manager.getBalance(f));
+        }
+
+        map.entrySet().stream()
+                .sorted(Map.Entry.<String, Double>comparingByValue().reversed())
+                .limit(10)
+                .forEach(e ->
+                        player.sendMessage("§e" + e.getKey() + " §7- §6" + e.getValue())
+                );
+
+        return true;
+    }
+}
