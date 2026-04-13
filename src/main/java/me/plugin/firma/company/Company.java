b9cd72cd100531d683b4c265e3ed219ef1f00fb5
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 15:26:40 +0200

    Company.java

--- src/main/java/me/plugin/firma/company/Company.java
+++ src/main/java/me/plugin/firma/company/Company.java
@@ -1,22 +1,30 @@
 package me.plugin.firma.company;
 
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
-public class Company implements Serializable {
+public class Company {
 
     public String name;
     public UUID owner;
+
     public double balance = 0;
+    public int level = 1;
+    public double xp = 0;
 
     public Map<UUID, Role> members = new HashMap<>();
-    public Map<UUID, String> jobs = new HashMap<>();
 
     public Company(String name, UUID owner) {
         this.name = name;
         this.owner = owner;
         members.put(owner, Role.OWNER);
+    }
+
+    public void addXP(double amount, int xpPerLevel) {
+        xp += amount;
+
+        if (xp >= xpPerLevel) {
+            xp = 0;
+            level++;
+        }
     }
 }
