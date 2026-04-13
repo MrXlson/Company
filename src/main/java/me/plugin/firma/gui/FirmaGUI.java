a566fe217d7acac3635a6656cc47937584a6788f
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 19:10:06 +0200

    FirmaGUI.java

--- src/main/java/me/plugin/firma/gui/FirmaGUI.java
+++ src/main/java/me/plugin/firma/gui/FirmaGUI.java
@@ -1,42 +1,41 @@
 package me.plugin.firma.gui;
 
 import me.plugin.firma.manager.FirmaManager;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.*;
-
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Arrays;
 
 public class FirmaGUI {
 
-    public static void openMainGUI(Player player, boolean hasCompany, String name, FirmaManager manager) {
+    public static void openMainGUI(Player p, boolean has, String name, FirmaManager manager) {
 
         Inventory inv = Bukkit.createInventory(null, 27, "§6BizCore");
 
-        if (!hasCompany) {
-            ItemStack item = new ItemStack(Material.EMERALD);
-            ItemMeta meta = item.getItemMeta();
-            meta.setDisplayName("§aZaložit firmu");
-            item.setItemMeta(meta);
-            inv.setItem(13, item);
+        if (!has) {
+            ItemStack create = new ItemStack(Material.EMERALD);
+            ItemMeta cm = create.getItemMeta();
+            cm.setDisplayName("§aZaložit firmu");
+            create.setItemMeta(cm);
+            inv.setItem(13, create);
         } else {
 
             ItemStack info = new ItemStack(Material.PAPER);
             ItemMeta im = info.getItemMeta();
-            im.setDisplayName("§eFirma: " + name);
+            im.setDisplayName("§e" + name);
             im.setLore(Arrays.asList(
-                    "§7Balance: " + manager.getBalance(name),
-                    "§7Level: " + manager.getLevel(name)
+                    "§7Balance: §a" + manager.getBalance(name),
+                    "§7Level: §b" + manager.getLevel(name),
+                    "§7XP: §e" + manager.getXP(name)
             ));
             info.setItemMeta(im);
 
             inv.setItem(13, info);
         }
 
-        player.openInventory(inv);
+        p.openInventory(inv);
     }
 }
