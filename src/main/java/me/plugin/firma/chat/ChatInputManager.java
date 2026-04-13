2298dc900c346c6c621f05bfd602e42d07f3f182
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 23:07:13 +0200

    ChatInputManager.java

--- src/main/java/me/plugin/firma/chat/ChatInputManager.java
+++ src/main/java/me/plugin/firma/chat/ChatInputManager.java
@@ -8,33 +8,27 @@ public class ChatInputManager {
     private final HashMap<UUID, String> waiting = new HashMap<>();
     private final HashMap<UUID, String> inputs = new HashMap<>();
 
-    // Nastaví hráče do čekání na input
     public void waitFor(UUID uuid, String type) {
         waiting.put(uuid, type);
     }
 
-    // Zkontroluje jestli hráč čeká na input
     public boolean isWaiting(UUID uuid) {
         return waiting.containsKey(uuid);
     }
 
-    // Získá typ inputu (např. "rename", "invite")
     public String getType(UUID uuid) {
         return waiting.get(uuid);
     }
 
-    // Uloží input
     public void set(UUID uuid, String value) {
         inputs.put(uuid, value);
         waiting.remove(uuid);
     }
 
-    // Získá input
     public String get(UUID uuid) {
         return inputs.get(uuid);
     }
 
-    // Vymaže input
     public void remove(UUID uuid) {
         inputs.remove(uuid);
         waiting.remove(uuid);
