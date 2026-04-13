703b08dac0eefffd77a5f0add6709fdf50b137ae
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 23:29:41 +0200

    FirmaPlugin.java

--- src/main/java/me/plugin/firma/FirmaPlugin.java
+++ src/main/java/me/plugin/firma/FirmaPlugin.java
@@ -1,29 +1,21 @@
 package me.plugin.firma;
 
-import me.plugin.firma.listener.FirmaListener;
 import me.plugin.firma.listener.ChatListener;
+import me.plugin.firma.listener.FirmaListener;
 import me.plugin.firma.manager.FirmaManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FirmaPlugin extends JavaPlugin {
 
-    private static FirmaPlugin instance;
     private FirmaManager manager;
 
     @Override
     public void onEnable() {
-        instance = this;
 
-        manager = new FirmaManager(this);
+        manager = new FirmaManager();
 
-        getServer().getPluginManager().registerEvents(new FirmaListener(this, manager), this);
+        getServer().getPluginManager().registerEvents(new FirmaListener(manager), this);
         getServer().getPluginManager().registerEvents(new ChatListener(manager), this);
-
-        getLogger().info("BizCore zapnut!");
-    }
-
-    public static FirmaPlugin getInstance() {
-        return instance;
     }
 
     public FirmaManager getManager() {
