0f4d76cae8b36d8d599f946e2ce27ff7c97c33f7
MrXlson <dominikdrasar39@gmail.com>
2026-04-14 00:01:14 +0200

    MainGUI.java

--- src/main/java/me/plugin/firma/gui/MainGUI.java
+++ src/main/java/me/plugin/firma/gui/MainGUI.java
@@ -1,27 +1,46 @@
 package me.plugin.firma.gui;
 
+import me.plugin.firma.manager.FirmaManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.*;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class MainGUI {
 
-    public static void open(Player p) {
-
+    public static void open(Player p, FirmaManager manager) {
         Inventory inv = Bukkit.createInventory(null, 27, "§6BizCore");
 
-        inv.setItem(11, item(Material.DIAMOND_PICKAXE, "§aPráce"));
-        inv.setItem(13, item(Material.PAPER, "§bČlenové"));
+        // 🔨 PRÁCE
+        ItemStack jobs = new ItemStack(Material.IRON_PICKAXE);
+        ItemMeta jobsMeta = jobs.getItemMeta();
+        jobsMeta.setDisplayName("§aPráce");
+        jobs.setItemMeta(jobsMeta);
+        inv.setItem(11, jobs);
+
+        // ⬆️ UPGRADE
+        ItemStack upgrade = new ItemStack(Material.EMERALD);
+        ItemMeta upgradeMeta = upgrade.getItemMeta();
+        upgradeMeta.setDisplayName("§bUpgrade");
+        upgrade.setItemMeta(upgradeMeta);
+        inv.setItem(13, upgrade);
+
+        // 📜 QUESTY
+        ItemStack quests = new ItemStack(Material.BOOK);
+        ItemMeta questMeta = quests.getItemMeta();
+        questMeta.setDisplayName("§eQuesty");
+        quests.setItemMeta(questMeta);
+        inv.setItem(15, quests);
+
+        // 👥 ČLENOVÉ
+        ItemStack members = new ItemStack(Material.PAPER);
+        ItemMeta membersMeta = members.getItemMeta();
+        membersMeta.setDisplayName("§dČlenové");
+        members.setItemMeta(membersMeta);
+        inv.setItem(22, members);
 
         p.openInventory(inv);
-    }
-
-    private static ItemStack item(Material mat, String name) {
-        ItemStack i = new ItemStack(mat);
-        ItemMeta meta = i.getItemMeta();
-        meta.setDisplayName(name);
-        i.setItemMeta(meta);
-        return i;
     }
 }
