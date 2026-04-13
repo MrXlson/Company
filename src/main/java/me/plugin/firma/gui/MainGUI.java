17d818fc9d8b8fc6e122ce87fd8d21609607ee68
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 23:53:04 +0200

    MainGUI.java

--- src/main/java/me/plugin/firma/gui/MainGUI.java
+++ src/main/java/me/plugin/firma/gui/MainGUI.java
@@ -1,1 +1,27 @@
+package me.plugin.firma.gui;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.*;
+
+public class MainGUI {
+
+    public static void open(Player p) {
+
+        Inventory inv = Bukkit.createInventory(null, 27, "§6BizCore");
+
+        inv.setItem(11, item(Material.DIAMOND_PICKAXE, "§aPráce"));
+        inv.setItem(13, item(Material.PAPER, "§bČlenové"));
+
+        p.openInventory(inv);
+    }
+
+    private static ItemStack item(Material mat, String name) {
+        ItemStack i = new ItemStack(mat);
+        ItemMeta meta = i.getItemMeta();
+        meta.setDisplayName(name);
+        i.setItemMeta(meta);
+        return i;
+    }
+}
