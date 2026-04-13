a41b1cb6312521e95ae4c65e2baec7413d268273
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 18:12:56 +0200

    QuestManager.java

--- src/main/java/me/plugin/firma/manager/QuestManager.java
+++ src/main/java/me/plugin/firma/manager/QuestManager.java
@@ -1,1 +1,20 @@
+package me.plugin.firma.manager;
 
+import java.util.HashMap;
+
+public class QuestManager {
+
+    private static final HashMap<String, Integer> progress = new HashMap<>();
+
+    public static void add(String firma) {
+        progress.put(firma, progress.getOrDefault(firma, 0) + 1);
+    }
+
+    public static int get(String firma) {
+        return progress.getOrDefault(firma, 0);
+    }
+
+    public static void reset(String firma) {
+        progress.remove(firma);
+    }
+}
