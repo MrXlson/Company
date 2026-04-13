4d851c264ec9d0a80a77c8479de364296d177dac
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 18:34:38 +0200

    ChatListener.java

--- src/main/java/me/plugin/firma/listener/ChatListener.java
+++ src/main/java/me/plugin/firma/listener/ChatListener.java
@@ -1,1 +1,35 @@
+package me.plugin.firma.listener;
 
+import me.plugin.firma.chat.ChatInputManager;
+import me.plugin.firma.manager.FirmaManager;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.entity.Player;
+
+public class ChatListener implements Listener {
+
+    private final FirmaManager manager;
+
+    public ChatListener(FirmaManager manager) {
+        this.manager = manager;
+    }
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+
+        Player player = e.getPlayer();
+
+        if (!ChatInputManager.isWaiting(player.getUniqueId())) return;
+
+        e.setCancelled(true);
+
+        String name = e.getMessage();
+
+        manager.createCompany(player.getUniqueId(), name);
+        ChatInputManager.remove(player.getUniqueId());
+
+        player.sendMessage("§aFirma vytvořena: " + name);
+    }
+}
