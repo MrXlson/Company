25e6ea63d23790ffd715e3c58068fb638a577720
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 19:06:39 +0200

    FirmaPlugin.java

--- src/main/java/me/plugin/firma/FirmaPlugin.java
+++ src/main/java/me/plugin/firma/FirmaPlugin.java
@@ -1,12 +1,9 @@
 package me.plugin.firma;
 
-import me.plugin.firma.command.*;
+import me.plugin.firma.command.FirmaCommand;
 import me.plugin.firma.listener.*;
 import me.plugin.firma.manager.FirmaManager;
-import me.plugin.firma.tasks.IncomeTask;
-import me.plugin.firma.economy.EconomySetup;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FirmaPlugin extends JavaPlugin {
@@ -18,25 +15,14 @@ public class FirmaPlugin extends JavaPlugin {
 
         saveDefaultConfig();
 
-        if (!EconomySetup.setup(this)) {
-            getLogger().severe("Vault nenalezen!");
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
-
         manager = new FirmaManager(this);
 
         getCommand("firma").setExecutor(new FirmaCommand(manager));
-        getCommand("firmatop").setExecutor(new FirmaTopCommand(manager));
 
         getServer().getPluginManager().registerEvents(new FirmaListener(manager), this);
         getServer().getPluginManager().registerEvents(new ChatListener(manager), this);
-        getServer().getPluginManager().registerEvents(new DamageListener(manager), this);
         getServer().getPluginManager().registerEvents(new QuestListener(manager), this);
 
-        Bukkit.getScheduler().runTaskTimer(this,
-                new IncomeTask(this, manager),
-                20 * 60,
-                20 * 60);
+        getLogger().info("BizCore V16 loaded!");
     }
 }
