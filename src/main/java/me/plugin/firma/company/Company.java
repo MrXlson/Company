27afd8e8a6c541b1924a13130854ccafed467814
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 14:47:55 +0200

    Company.java

--- src/main/java/me/plugin/firma/company/Company.java
+++ src/main/java/me/plugin/firma/company/Company.java
@@ -1,1 +1,22 @@
-implements Serializable
+package me.plugin.firma.company;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Company implements Serializable {
+
+    public String name;
+    public UUID owner;
+    public double balance = 0;
+
+    public Map<UUID, Role> members = new HashMap<>();
+    public Map<UUID, String> jobs = new HashMap<>();
+
+    public Company(String name, UUID owner) {
+        this.name = name;
+        this.owner = owner;
+        members.put(owner, Role.OWNER);
+    }
+}
