a0ef61d805e6119a8118d9ddd79fb7905c1fe555
MrXlson <dominikdrasar39@gmail.com>
2026-04-14 00:14:14 +0200

    FirmaPlugin.java

--- src/main/java/me/plugin/firma/FirmaPlugin.java
+++ src/main/java/me/plugin/firma/FirmaPlugin.java
@@ -6,34 +6,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class FirmaPlugin extends JavaPlugin {
 
-    private static FirmaPlugin instance;
     private FirmaManager manager;
 
     @Override
     public void onEnable() {
 
-        instance = this;
+        manager = new FirmaManager(this);
 
-        // 📦 inicializace manageru
-        manager = new FirmaManager();
+        getServer().getPluginManager().registerEvents(
+                new InventoryListener(manager),
+                this
+        );
 
-        // 📡 registrace listeneru
-        getServer().getPluginManager().registerEvents(new InventoryListener(manager), this);
-
-        getLogger().info("FirmaPlugin byl zapnut!");
+        getLogger().info("Plugin běží");
     }
 
-    @Override
-    public void onDisable() {
-        getLogger().info("FirmaPlugin byl vypnut!");
-    }
-
-    // 🔹 přístup k instanci pluginu
-    public static FirmaPlugin getInstance() {
-        return instance;
-    }
-
-    // 🔹 přístup k manageru
     public FirmaManager getManager() {
         return manager;
     }
