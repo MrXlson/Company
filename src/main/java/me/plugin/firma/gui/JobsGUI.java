8614cf7429b1bffbbd90c8bd1b7c5215c2e5ea93
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 23:53:31 +0200

    JobsGUI.java

--- src/main/java/me/plugin/firma/gui/JobsGUI.java
+++ src/main/java/me/plugin/firma/gui/JobsGUI.java
@@ -1,1 +1,26 @@
+package me.plugin.firma.gui;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.*;
+
+public class JobsGUI {
+
+    public static void open(Player p) {
+
+        Inventory inv = Bukkit.createInventory(null, 27, "§aPráce");
+
+        inv.setItem(13, item(Material.DIAMOND_PICKAXE, "§aKopání = peníze"));
+
+        p.openInventory(inv);
+    }
+
+    private static ItemStack item(Material mat, String name) {
+        ItemStack i = new ItemStack(mat);
+        ItemMeta meta = i.getItemMeta();
+        meta.setDisplayName(name);
+        i.setItemMeta(meta);
+        return i;
+    }
+}
