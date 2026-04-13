e51662b52858947574aa3370f2934aff370f570f
MrXlson <dominikdrasar39@gmail.com>
2026-04-13 14:43:21 +0200

    BankService.java

--- src/main/java/me/plugin/firma/economy/BankService.java
+++ src/main/java/me/plugin/firma/economy/BankService.java
@@ -1,1 +1,32 @@
 
+package me.plugin.firma.economy;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.entity.Player;
+
+public class BankService {
+
+    private final Economy econ;
+
+    public BankService(Economy econ) {
+        this.econ = econ;
+    }
+
+    public boolean deposit(Player p, double amount) {
+        if (econ == null) return false;
+
+        if (econ.getBalance(p) < amount) {
+            p.sendMessage("§cNemáš dost peněz!");
+            return false;
+        }
+
+        econ.withdrawPlayer(p, amount);
+        return true;
+    }
+
+    public void withdraw(Player p, double amount) {
+        if (econ == null) return;
+
+        econ.depositPlayer(p, amount);
+    }
+}
